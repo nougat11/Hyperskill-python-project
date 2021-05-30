e9e054fc0430cfb52f4c7305f27177998dff78ef
nougat11 <30313702+nougat11@users.noreply.github.com>
2021-05-31 00:20:25 +0300

    bill splitter: final
    divide bill between non-lucky persons

--- easy/Bill Splitter/main.py
+++ easy/Bill Splitter/main.py
@@ -1,3 +1,4 @@
+# write your code here
 import random
 number = int(input("Enter the number of friends joining (including you): "))
 print()
@@ -11,15 +12,20 @@ else:
     print()
     print("Enter the total bill value: ")
     money = int(input())
-    money = round(money / number, 2)
-    guests = dict.fromkeys(names, money)
+      
+    guests = dict.fromkeys(names, round(money / number, 2))
     print()
     print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
     action = input()
     print()
     if action == "Yes":
-        
-        print(f"{random.choice(names)} is the lucky one!")
+        name = random.choice(names)
+        print(f"{name} is the lucky one!")
+        for key, value in guests.items():
+            if key == name:
+                guests[key] = 0
+            else:
+                guests[key] = round(money / (number - 1), 2)
     elif action == "No":
-        
         print("No one is going to be lucky")
+    print(guests)
