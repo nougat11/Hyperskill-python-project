414656335ad75a9bb0471431c66d2e4bec3045c5
nougat11 <30313702+nougat11@users.noreply.github.com>
2021-05-31 00:48:14 +0300

    simple chat bot: stage 4
    feat:
    count from 0 to n

--- easy/simple chat bot/main.py
+++ easy/simple chat bot/main.py
@@ -8,9 +8,17 @@ print('What a great name you have, ' + name + '!')
 print('Let me guess your age.')
 print('Enter remainders of dividing your age by 3, 5 and 7.')
 
+rem3 = int(input())
+rem5 = int(input())
+rem7 = int(input())
 
-remainder3 = int(input())
-remainder5 = int(input())
-remainder7 = int(input())
-age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
-print(f"Your age is {age}; that's a good time to start programming!")
+age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+print("Your age is " + str(age) + "; that's a good time to start programming!")
+print('Now I will prove to you that I can count to any number you want.')
+count = int(input())
+for i in range(count + 1):
+    print(f'{i} !')
+
+
+print('Completed, have a nice day!')
