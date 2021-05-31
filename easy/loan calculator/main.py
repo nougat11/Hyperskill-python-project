af0314548eab0098ec5dea75a1b482826c4122d0
nougat11 <30313702+nougat11@users.noreply.github.com>
2021-05-31 23:10:45 +0300

    loan calculator: stage 2
    calculate month to repay the loan, and montly payment

--- easy/loan calculator/main.py
+++ easy/loan calculator/main.py
@@ -1,12 +1,25 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
-
-# write your code here
-print(loan_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+import math
+print('Enter the loan principal:')
+loan_principal = int(input())
+print("""
+What do you want to calculate?
+type "m" for number of monthly payments,
+type "p" for the monthly payment
+""")
+action = input()
+if action == 'm':
+    print("Enter the monthly payment:")
+    payment = int(input())
+    months = math.ceil(loan_principal / payment)
+    if months != 1:
+        print(f"It will take {months} months to repay the loan")
+    else:
+        print(f"It will take {months} month to repay the loan")
+else:
+    print("Enter the number of months:")
+    months = int(input())
+    payment = math.ceil(loan_principal / months)
+    if months % payment == 0:
+        print(f"Your monthly payment = {payment}")
+    else:
+        print(f"Your monthly payment = {payment} and the last payment = {loan_principal - payment * (months - 1)}.")
